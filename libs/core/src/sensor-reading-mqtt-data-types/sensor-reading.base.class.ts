45af9b68af5aa729bdd40e640cfdc3c9fde811bb
Stefaan Vandevelde <stefaan.vandevelde@bruyland.be>
2022-09-01 10:05:20 +0200

    sensor-reading base

--- libs/core/src/sensor-reading-mqtt-data-types/sensor-reading.base.class.ts
+++ libs/core/src/sensor-reading-mqtt-data-types/sensor-reading.base.class.ts
@@ -1,28 +1,49 @@
 export type SensorReadingValueBaseType = object | string | number | boolean | null
 export type ValueFormatter = (value: SensorReadingValueBaseType, unit: string) => string
 
+export class SensorReadingMqttDataTypeSettings<TTypeIndicator> {
+  constructor(
+    readonly type: TTypeIndicator,
+    readonly unit: string,
+    readonly formatter: undefined | number | ValueFormatter,
+  ) {}
+}
 export abstract class SensorReadingMqttData_base_class<TValue extends SensorReadingValueBaseType, TTypeIndicator> {
   origin: string
   time: Date
   name: string
-  unit: string
   formattedValue: string
   value: TValue
-  type: TTypeIndicator
+  typeSettings: SensorReadingMqttDataTypeSettings<TTypeIndicator>
 
-  constructor(
-    value: TValue,
-    name: string,
-    origin: string,
-    unit: string = '',
-    formatter: ValueFormatter = (value: SensorReadingValueBaseType) => value.toString(),
-    time: Date = new Date(),
-  ) {
+  constructor(value: TValue, name: string, origin: string, time: Date = new Date()) {
     this.value = value
     this.name = name
     this.time = time
-    this.unit = unit
     this.origin = origin
-    this.formattedValue = formatter(value, unit)
+    this.typeSettings
+  }
+
+  toString() {
+    const formatter = this.typeSettings.formatter
+    let formattedValue = ''
+    switch (typeof formatter) {
+      case 'undefined': // no formatter defined for string type values
+        formattedValue = this.value as string
+        break
+      case 'number':
+        formattedValue = `${(formatter as number).toFixed(formatter)} ${this.typeSettings.unit}`
+        break
+      case 'function':
+        formattedValue = formatter(this.value, this.typeSettings.unit)
+    }
+    const { typeSettings, ...data } = {
+      type: this.typeSettings.type,
+      ...this,
+      unit: this.typeSettings.unit,
+      formattedValue,
+    }
+
+    return JSON.stringify(data)
   }
 }
