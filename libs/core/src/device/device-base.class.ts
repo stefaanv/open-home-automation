01d1bc5611ff0d85ff17307fe5055ba82c2919b4
Stefaan Vandevekde <stefaan.vandevelde.kortrijk@gmail.com>
2022-09-03 19:59:33 +0200

    Type system update

--- libs/core/src/device/device-base.class.ts
+++ libs/core/src/device/device-base.class.ts
@@ -1,38 +1,11 @@
-import { SensorValueTypeSettings } from '@core/sensor-reading-mqtt-data-types/sensor-reading.base.class'
+import { SensorReadingMqttData_base_class, SensorReadingValueBaseType } from '@core/sensor-reading-mqtt-data-types/sensor-reading.base.class'
 
 /** specifieke sensor of actuator */
-export class DeviceBase<TUID extends number | string> {
+export interface DeviceBase<TUID extends number | string, TConfig, TValue extends SensorReadingValueBaseType, TValueTypeIndicator extends string> {
   readonly uid: TUID
-  readonly type: 'sensor' | 'actuator'
   readonly name: string
+  readonly config: TConfig
+  readonly mqttValue: SensorReadingMqttData_base_class<TValue, TValueTypeIndicator>
+  transformer: (state: any) => TValue
 }
 
-export class SensorBase<TUID extends number | string, TValueTypeIndicator extends string> extends DeviceBase<TUID> {
-  type: 'sensor' = 'sensor'
-  constructor(readonly valueTypeSettings: SensorValueTypeSettings<TValueTypeIndicator>) {
-    super()
-  }
-}
-
-export class ActuatorBase<TUID extends number | string> {
-  type: 'actuator' = 'actuator'
-}
-
-export class DeviceList<TUID extends number | string> {
-  private readonly _list: DeviceBase<TUID>[] = []
-
-  public push(args: DeviceBase<TUID> | Array<DeviceBase<TUID>>) {
-    const array: DeviceBase<TUID>[] = args.hasOwnProperty('name') ? [args as DeviceBase<TUID>] : (args as DeviceBase<TUID>[])
-    array.forEach(element => {
-      this._list.push(element)
-    })
-  }
-
-  public get(uid: TUID): DeviceBase<TUID> | undefined {
-    return this._list.find(e => e.uid === uid)
-  }
-
-  public getAll(): DeviceBase<TUID>[] {
-    return this._list
-  }
-}
