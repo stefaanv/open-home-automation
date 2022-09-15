d0d317a06676a878ac302f89b262fb2eda9ec042
Stefaan Vandevelde <stefaan.vandevelde@bruyland.be>
2022-09-15 10:06:39 +0200

    NumericMeasurementTypeEnum onderdeel van MeasurementTypeEnum

--- libs/core/src/measurement-type.enum.ts
+++ libs/core/src/measurement-type.enum.ts
@@ -1,14 +1,4 @@
-export type MeasurementTypeEnum =
-  | 'temperature'
-  | 'humidity'
-  | 'switch'
-  | 'on-off'
-  | 'contact'
-  | 'illuminance'
-  | 'presence'
-  | 'air-quality'
-  | 'closure'
-  | 'moving'
-  | 'open-closed'
+export type MeasurementTypeEnum = NumericMeasurementTypeEnum &
+  ('switch' | 'on-off' | 'contact' | 'presence' | 'air-quality' | 'moving' | 'open-closed')
 
 export type NumericMeasurementTypeEnum = 'temperature' | 'humidity' | 'illuminance' | 'closure'
